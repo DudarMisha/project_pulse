07b851f7e06bc2593c719a5523c05e3a91a27662
Misha <dudarm7@gmail.com>
2020-03-19 14:04:53 +0200

    ready slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,29 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1200,
-       /*  adaptiveHeight: true, */
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/elements_slider/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/elements_slider/right.svg"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
-            }
-        ]
-    });
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false,
+    responsive: {
+        320: {
+          edgePadding: 20,
+          gutter: 20,
+          items: 1,
+          nav: true
+        },
+        768: {
+          items: 1,
+          nav: false
+        }
+    }
 });
+
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+});
+
