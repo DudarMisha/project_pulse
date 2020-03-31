bb4d18cecab4f5a079df1f3404613c50e655cada
Misha <dudarm7@gmail.com>
2020-03-31 17:17:34 +0300

    adaptive is oll validation forms is ready mask in form is ready

--- src/js/script.js
+++ src/js/script.js
@@ -59,3 +59,40 @@ $('.button_mini').each(function(i) {
       $('.overlay, #order').fadeIn('slow');
     })
 });
+
+//validation form
+
+function validateForms(form){
+  $(form).validate({
+      rules: {
+          name: {
+              required: true,
+              minlength: 2
+          },
+          phone: "required",
+          email: {
+              required: true,
+              email: true
+          }
+      },
+      messages: {
+          name: {
+              required: "Пожалуйста, введите свое имя",
+              minlength: jQuery.validator.format("Введите {0} символа!")
+            },
+          phone: "Пожалуйста, введите свой номер телефона",
+          email: {
+            required: "Пожалуйста, введите свою почту",
+            email: "Неправильно введен адрес почты"
+          }
+      }
+  });
+};
+
+validateForms('#consultation-form');
+validateForms('#consultation form');
+validateForms('#order form');
+
+//pone number musk
+
+  $('input[name=phone]').mask("+7 (999) 999-99-99");
