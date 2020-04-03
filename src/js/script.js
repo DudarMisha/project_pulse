ba28d0c80b176dcb4e107c09c7984700ad49e59d
Misha <dudarm7@gmail.com>
2020-04-03 18:19:12 +0300

    add mailer folder

--- src/js/script.js
+++ src/js/script.js
@@ -95,4 +95,20 @@ validateForms('#order form');
 
 //pone number musk
 
-  $('input[name=phone]').mask("+7 (999) 999-99-99");
+$('input[name=phone]').mask("+7 (999) 999-99-99");
+
+$('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thanks').fadeIn('slow');
+
+        $('form').trigger('reset');
+    });
+    return false;
+});
